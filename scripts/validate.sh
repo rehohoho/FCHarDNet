dbe653ac5a09037692ce419e8f23c329b67ca6f1
Ho Rui En <ernie.birdy.ho@gmail.com>
2020-03-18 20:15:20 +0800

    Add script to copy images according to csv from validationn

---
+++ scripts/validate.sh
@@ -0,0 +1,4 @@
+
+python ../validate.py \
+    --config=../configs/scooter_only.yml \
+    --model_path=../runs/scooter_only_labelled/cur/hardnet_base_cityscapes_convention_best_model.pkl
---
+++ scripts/validate_show_images.sh
@@ -0,0 +1,5 @@
+
+python /home/whizz/Desktop/deeplabv3/FCHarDNet/validate_show_images.py \
+    --dataset_path="/home/whizz/Desktop/deeplabv3/datasets/scooter/scooter_images/val/" \
+    --csv_path="/home/whizz/Desktop/deeplabv3/FCHarDNet/runs/scooter_only_labelled/cur/miou_logs.csv" \
+    --perc=0.1
